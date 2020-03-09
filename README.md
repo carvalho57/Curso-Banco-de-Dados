820fedc86d1e5513997c0f49d130584f2a4cf2d9
carvalho57 <gabrielfeocarvalho57@gmail.com>
2020-03-09 10:32:47 -0300

    Modelando sistema seu jose

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Aprendendo-Banco-de-Dados
+# Aprendendo Banco de Dados
+ "O curso completo e Banco de Dados e SQL, sem mistérios!"
+
+
+## Visto ate agora
+- [X] O que é o banco de dados
+- [X] Modelagem
+- [X] Comandos de DDL
+- [X] Comandos de DML
+- [X] Primeira Forma Normal
+- [] JOIN
+- [] Views
+- [] Procedures
---
+++ Sistema_SeuJose/ModelagemSistema.sql
@@ -0,0 +1,19 @@
+/*  Requisitos
+
+    Carros entram para realizar serviços e os seus respectivos donos.
+    Cada cliente possui apenas um carro
+    Carro possui uma marca
+    Cores dos carros para ter idéia de qual tinta comprar
+    Carro pode ter mais de uma cor
+    Necessita armazenar telefones dos clientes
+    mas não são obrigatórios;
+
+    Inicio da Modelagem
+
+    Cliente -> Carro
+    Cliente -> Telefone
+    Carro ->  Marca
+    Carro ->  mais de uma cor
+    
+*/
+
